54c3ea9ca2396c4b3220197720adaa4cad8a4e36
Manoj_K <manoj.bk14@gmail.com>
2017-11-10 13:02:05 +0530

    Update ZipFolders.sh

--- ZipFolders.sh
+++ ZipFolders.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 # This Scripts Zips all the folder starting prefix "Invoice" , EX:INVOICE_20171011 to INVOICE_20171011.zip
+#This Script to be added in cron job at UNIX level to run every 10 mins . Nothing to be mentioned in Channel Level. 
 echo "Zip all directories with starting with Prefix Invoice from path /informatica/Ariba/AX/Child_Trans_Invoice/Inbound/Download/" 
 for file  in $(ls /informatica/Ariba/AX/Child_Trans_Invoice/Inbound/Download/ |grep Invoice |grep -v .zip)
 do
